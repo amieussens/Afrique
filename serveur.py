1df45ee042ac781fb5273543e66f1ab98b6ca60f
Sall <aliouibnibrahim@gmail.com>
2020-06-16 09:59:24 +0200

    Implementation of HEAD method and sending static document

--- serveur.py
+++ serveur.py
@@ -16,7 +16,7 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
   server_version = 'countries/0.1'
 
   #
-  # Surcharge la méthode qui traite les requêtes GET
+  # Surcharge de la méthode qui traite les requêtes GET
   #
   def do_GET(self):
     # Récupération des paramètres
@@ -57,3 +57,22 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     # ou pas...
     else:
       self.send_static()
+
+  # Surcharge de la méthode qui traite les requêtes HEAD
+
+  def do_HEAD(self):
+    self.send_static()
+
+  # Envoi du document statique demandé
+
+  def send_static(self):
+
+    # on modifie le chemin d'accès en insérant un répertoire préfixe
+    self.path = self.static_dir + self.path
+
+    # on appelle la méthode parent (do_GET ou do_HEAD)
+    # à partir du verbe HTTP (GET ou HEAD)
+    if (self.command=='HEAD'):
+        http.server.SimpleHTTPRequestHandler.do_HEAD(self)
+    else:
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
